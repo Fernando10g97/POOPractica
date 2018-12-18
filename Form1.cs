c26ba15dd4b7fef9e0bda128efcbcd503363cb82
fernando10g97 <fernando10g97@gmail.com>
2018-12-18 07:58:44 -0500

    se agrego un formulario

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace github
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
